296d471772880674804027c5581b66e5e8429ed7
Magnus Sundström <kmvsundstrom@gmail.com>
2018-08-22 14:30:59 +0200

    Added functionality to Index page

--- BussKollen/BussKollen/Views/MainPage.xaml.cs
+++ BussKollen/BussKollen/Views/MainPage.xaml.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-
+using BussKollen.Models;
+using BussKollen.Services;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +11,39 @@ namespace BussKollen.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
+        private TravelService _service;
+
         public MainPage()
         {
+            _service = new TravelService();
             InitializeComponent();
+
+            StartLocation.TextChanged += StartLocation_TextChanged;
+        }
+
+        private async void StartLocation_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (StartLocation.Text.Length > 2)
+            {
+                var locationList = await GetLocation();
+
+                if (locationList != null)
+                {
+                    ShowLocationList(locationList);
+                }
+
+                return;
+            }
+        }
+
+        private async Task<List<LocationDTO>> GetLocation()
+        {
+            return await _service.GetLocation(StartLocation.Text);
+        }
+
+        private void ShowLocationList(List<LocationDTO> list)
+        {
+            Navigation.PushAsync(new LocationListPage(list));
         }
     }
 }
