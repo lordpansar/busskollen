c9c659ccc6343f8a82f5f2138b4736905093a728
Magnus Sundström <kmvsundstrom@gmail.com>
2018-08-22 14:32:04 +0200

    Added LocationList view

---
+++ BussKollen/BussKollen/Views/LocationListPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using BussKollen.Models;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BussKollen.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LocationListPage : ContentPage
+    {
+        public LocationListPage(List<LocationDTO> locations)
+        {
+            InitializeComponent();
+            locationList.ItemsSource = locations;
+            locationList.ItemTapped += LocationListPage_OnItemTapped;
+        }
+
+        private void LocationListPage_OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            Navigation.PopToRootAsync();
+        }
+    }
+}
