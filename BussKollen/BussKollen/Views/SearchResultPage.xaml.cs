625f746468f0eab40b342568d4c7cae912bec6ff
Magnus Sundström <kmvsundstrom@gmail.com>
2018-08-23 15:12:03 +0200

    Added SearchResultPage view

---
+++ BussKollen/BussKollen/Views/SearchResultPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using BussKollen.Models;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BussKollen.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SearchResultPage : ContentPage
+    {
+        public SearchResultPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
