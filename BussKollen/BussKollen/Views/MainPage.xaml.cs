30d45df241aef709e20f36d14ce4869bd379f199
Magnus Sundström <kmvsundstrom@gmail.com>
2018-08-23 15:09:47 +0200

    Added functionality for selecting locations

--- BussKollen/BussKollen/Views/MainPage.xaml.cs
+++ BussKollen/BussKollen/Views/MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using BussKollen.Models;
@@ -12,38 +12,92 @@ namespace BussKollen.Views
     public partial class MainPage : ContentPage
     {
         private TravelService _service;
+        private int startLocationID;
+        private int finalLocationID;
 
         public MainPage()
         {
             _service = new TravelService();
             InitializeComponent();
-
-            StartLocation.TextChanged += StartLocation_TextChanged;
+            
+            //Event handlers
+            TxtStartLocation.TextChanged += StartLocation_TextChanged;
+            StartLocationPicker.SelectedIndexChanged += StartPicker_Selected;
+            FinalLocationPicker.SelectedIndexChanged += FinalPicker_Selected;
+            BtnSearch.Clicked += BtnSearch_Clicked;
         }
 
         private async void StartLocation_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (StartLocation.Text.Length > 2)
+            if (TxtStartLocation.Text.Length > 2)
             {
-                var locationList = await GetLocation();
-
-                if (locationList != null)
-                {
-                    ShowLocationList(locationList);
-                }
-
-                return;
+                var locationList = await GetLocation(TxtStartLocation.Text);
+                BindLocationPicker(LblWarningStart, StartLocationPicker, locationList);
             }
         }
 
-        private async Task<List<LocationDTO>> GetLocation()
+        private void BindLocationPicker(Label label, Picker picker, List<LocationDTO> locationList)
         {
-            return await _service.GetLocation(StartLocation.Text);
+            if (locationList != null)
+            {
+                picker.ItemsSource = locationList;
+                picker.IsVisible = true;
+                label.IsVisible = false;
+            }
+            else
+            {
+                label.Text = "Inga träffar :(";
+                label.IsVisible = true;
+                picker.ItemsSource = null;
+                picker.IsVisible = false;
+            }
         }
 
-        private void ShowLocationList(List<LocationDTO> list)
+        private void StartPicker_Selected(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new LocationListPage(list));
+            TxtStartLocation.Text = "";
+            startLocationID = GetLocationID(StartLocationPicker);
+            ToggleSearchButton();
+        }
+
+        private void FinalPicker_Selected(object sender, EventArgs e)
+        {
+            TxtFinalLocation.Text = "";
+            finalLocationID = GetLocationID(StartLocationPicker);
+            ToggleSearchButton();
+        }
+
+        private void BtnSearch_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int GetLocationID(Picker picker)
+        {
+            var location = (LocationDTO)FinalLocationPicker.SelectedItem;
+            return location.Id;
+        }
+
+        private async Task<List<LocationDTO>> GetLocation(string query)
+        {
+            return await _service.GetLocation(query);
+        }
+
+        private void ShowResults()
+        {
+            Navigation.PushAsync(new SearchResultPage());
+        }
+
+        private void ToggleSearchButton()
+        {
+            if (startLocationID > 0 && finalLocationID > 0 && startLocationID != finalLocationID)
+            {
+                BtnSearch.IsEnabled = true;
+            }
+            else
+            {
+                BtnSearch.IsEnabled = false;
+            }
         }
     }
 }
